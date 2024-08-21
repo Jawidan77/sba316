e09de9c372142d4b4a04af787b8516a472049a9c
Jawidan77 <jawidan.reza77@gmail.com>
2024-08-21 15:53:40 -0400

    sba16

---
+++ .history/sba316_20240821145629.js
@@ -0,0 +1,29 @@
+document.getElementById('task-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const taskInput = document.getElementById('task-input');
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== '') {
+        const li = document.createElement('li');
+        li.textContent = taskText;
+
+        const removeBtn = document.createElement('button');
+        removeBtn.textContent = 'Remove';
+        removeBtn.addEventListener('click', function() {
+            li.remove();
+        });
+
+        li.appendChild(removeBtn);
+        document.getElementById('task-list').appendChild(li);
+
+        taskInput.value = '';
+    }
+});
+
+let myWindow;
+
+function newWindow () {
+    mywindow =window
+
+}
---
+++ .history/sba316_20240821150828.js
@@ -0,0 +1,39 @@
+document.getElementById('task-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const taskInput = document.getElementById('task-input');
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== '') {
+        const li = document.createElement('li');
+        li.textContent = taskText;
+
+        const removeBtn = document.createElement('button');
+        removeBtn.textContent = 'Remove';
+        removeBtn.addEventListener('click', function() {
+            li.remove();
+        });
+
+        li.appendChild(removeBtn);
+        document.getElementById('task-list').appendChild(li);
+
+        taskInput.value = '';
+    }
+});
+
+let myWindow;
+
+function newWindow () {
+    mywindow =window.open(
+        "https://zty.pe/",
+        "gams",
+        "Width=750, height=400, resizeable=yes, scrollbars=yes, location=yes",
+
+    );
+    myWindow.focus();
+
+    function closeWindow() {
+    }
+    document.getElementById("openwindowBtn").addEventListener
+
+}
